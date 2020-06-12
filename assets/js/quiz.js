906f9dafbd01075ec04d444582a8cc39154f5d40
brian presta <brian.presta@gmail.comgit commit -m first>
2020-06-12 12:17:07 -0700

    quiz buttons now know whether they're the right answer

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,29 +1,32 @@
-var pageContentE1 = document.querySelector("body")
-var highScoreButtonE1 = pageContentE1.querySelector("#high-score")
-var timerE1 = pageContentE1.querySelector("#timer")
-var startButtonE1 = pageContentE1.querySelector("#start")
-var introE1 = pageContentE1.querySelector("#intro")
-var quizE1 = pageContentE1.querySelector("#quiz")
-var doneScreenE1 = pageContentE1.querySelector("#done")
-var time = 5
+var pageContentE1 = document.querySelector("body");
+var highScoreButtonE1 = pageContentE1.querySelector("#high-score");
+var timerE1 = pageContentE1.querySelector("#timer");
+var startButtonE1 = pageContentE1.querySelector("#start");
+var introE1 = pageContentE1.querySelector("#intro");
+var quizE1 = pageContentE1.querySelector("#quiz");
+var doneScreenE1 = pageContentE1.querySelector("#done");
+var time = 50;
+var questionTracker = 0;
 var questionHolder = [
     {
         question: "Which of the following is not a data type in JavaScript?",
-        answer1: "String",
-        answer2: "Array",
-        answer3: "Float",
-        answer4: "Boolean",
-        correctAnswer: "answer3"
+        answers: [
+            {text:"1. String", correct:false},
+            {text:"2. Array", correct:false},
+            {text:"3. Float", correct:true},
+            {text:"4. Boolean", correct:false}
+        ]
     },
     {
         question: "Which of the following could be used to turn a string into an array?",
-        answer1: "string.split()",
-        answer2: "string.toArray()",
-        answer3: "toArray(string)",
-        answer4: "split(string)",
-        correctAnswer: "answer1"
+        answers: [
+            {text:"1. string.split()", correct:true},
+            {text:"2. string.toArray()", correct:false},
+            {text:"3. toArray(string)", correct:false},
+            {text:"4. split(string)", correct:false}
+        ]
     }
-]
+];
 var clockStart = function(){    
     var countDownProcessor = function(){
         if (time < 1) {
@@ -37,11 +40,26 @@ var clockStart = function(){
         time --
     }
     var timerCountdown = setInterval(countDownProcessor,1000)
+};
+var drawQuiz = function(){
+    if (questionTracker >= questionHolder.length) {
+        time = 0
+        return
+    }
+    var quizButtons = quizE1.querySelector(".quiz-button-wrapper").children
+    var currentQuestion = questionHolder[questionTracker]
+    quizE1.querySelector("h2").textContent = currentQuestion.question
+    for (x=0;x<4;x++){
+        quizButtons[x].textContent = currentQuestion.answers[x].text 
+        quizButtons[x].setAttribute("data-correct",currentQuestion.answers[x].correct)
+        console.log(quizButtons[x].getAttribute("data-correct"))
+    }
 }
 var startQuiz = function(){
     introE1.style = "display: none"
     quizE1.style = null
     clockStart()
-}
+    drawQuiz()
+};
 
-startButtonE1.addEventListener("click",startQuiz)
+startButtonE1.addEventListener("click",startQuiz);
