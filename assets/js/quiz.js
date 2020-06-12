8e5610e4defe740fcb785eb0d503b746186aba1e
brian presta <brian.presta@gmail.comgit commit -m first>
2020-06-11 17:13:36 -0700

    added basic timer countdown

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,1 +1,12 @@
 var timerE1 = document.querySelector("#timer")
+var time = 5
+var clockStart = function(){    
+    var countDownProcessor = function(){
+        timerE1.textContent = "Time: " + time
+        time --
+        if (time < 0) {
+            clearInterval(timerCountdown)
+        }
+    }
+    var timerCountdown = setInterval(countDownProcessor,1000)
+}
