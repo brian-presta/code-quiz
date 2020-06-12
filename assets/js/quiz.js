895f73ca916a78a834d47212365d1d0472b1477f
brian presta <brian.presta@gmail.comgit commit -m first>
2020-06-12 16:29:43 -0700

    built out high score screen and related functions

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,11 +1,14 @@
 var pageContentE1 = document.querySelector("body");
-var highScoreButtonE1 = pageContentE1.querySelector("#high-score");
-var timerE1 = pageContentE1.querySelector("#timer");
-var startButtonE1 = pageContentE1.querySelector("#start");
 var introE1 = pageContentE1.querySelector("#intro");
 var quizScreenE1 = pageContentE1.querySelector("#quiz");
+var doneScreenE1 = pageContentE1.querySelector(".done-screen");
+var highScoreScreenE1 = pageContentE1.querySelector(".high-score-screen")
+var highScoreLinkE1 = pageContentE1.querySelector("#high-score-link");
+var backButtonE1 = highScoreScreenE1.querySelector("#back")
+var clearScoresButtonE1 = highScoreScreenE1.querySelector("#clear-score")
+var startButtonE1 = pageContentE1.querySelector("#start");
+var initialsFormE1 = doneScreenE1.querySelector("form")
 var quizButtonsHolder = quizScreenE1.querySelector(".quiz-button-wrapper");
-var doneScreenE1 = pageContentE1.querySelector("#done");
 var time = 50;
 var questionTracker = 0;
 var questionHolder = [
@@ -29,18 +32,23 @@ var questionHolder = [
     }
 ];
 var clockStart = function(){    
+    var timerE1 = pageContentE1.querySelector("#timer")
     var countDownProcessor = function(){
-        if (time < 1) {
-            timerE1.textContent = "Time: 0"
+        // time in the deep negatives means the quiz was ended for a reason besides timing out, 
+        // the timer does not need to call endQuiz in those situations
+        if (time < -100){
             clearInterval(timerCountdown)
-            quizScreenE1.style = "display: none"
-            doneScreenE1.style = null
-            
+            return
+        }
+        if (time < 1) {
+            clearInterval(timerCountdown)
+            endQuiz()
         }
         timerE1.textContent = "Time: " + time
         time --
     }
     var timerCountdown = setInterval(countDownProcessor,1000)
+    
 };
 var quizButtonHandler = function(event) {
     var button = event.target.closest(".button")
@@ -48,30 +56,36 @@ var quizButtonHandler = function(event) {
         return
     }
     evalQuiz(button)
-}
+};
 var evalQuiz = function(button) {
+    // checks if there's previous feedback text, removes it if so
     var feedback = pageContentE1.querySelector("#feedback")
     if (feedback){
         feedback.remove()
     }
+    //  creates feedback text for user
     feedback = document.createElement("h3")
     feedback.id = "feedback"
     feedback.textContent = "Your last answer was "
+    // checks if their answer was right or wrong, changes feedback accordingly
     if (button.getAttribute("data-correct") === 'true'){
         feedback.textContent += "correct!"
     }
     else {
+        // if wrong, docks them time
         time -= 20
         feedback.textContent += "incorrect!"
     }
+    // puts feedback on page
     pageContentE1.querySelector(".feedback-holder").appendChild(feedback)
-
+    // checks if there are any more questions, ends the quiz if there aren't
     if (questionTracker >= questionHolder.length) {
-        time = 0
+        endQuiz()
         return
     }
+    // goes to the next question
     drawQuiz()
-}
+};
 var drawQuiz = function(){
     var quizButtons = quizButtonsHolder.children
     var currentQuestion = questionHolder[questionTracker]
@@ -81,14 +95,43 @@ var drawQuiz = function(){
         quizButtons[x].setAttribute("data-correct",currentQuestion.answers[x].correct)
     }
     questionTracker++
-}
+};
 var startQuiz = function(event){
     introE1.style = "display: none"
     quizScreenE1.style = null
     clockStart()
     drawQuiz()
 };
+var endQuiz = function(){
+    var score = time
+    time = -200
+    quizScreenE1.style = "display: none"
+    doneScreenE1.style = null
+    pageContentE1.querySelector("#timer").textContent = "Time: 0"
+    doneScreenE1.querySelector("p").textContent += (score + ".")
+};
+var submitScore = function(){
+    toHighScore()
+}
+var toHighScore = function(){
+    time = -200
+    pageContentE1.querySelector("#timer").textContent = "Time: 0"
+    introE1.style = "display: none"
+    quizScreenE1.style = "display: none"
+    doneScreenE1.style = "display: none"
+    pageContentE1.querySelector(".feedback-holder").style = "display: none"
+    highScoreScreenE1.style = null
+    drawHighScores()
+};
+var drawHighScores = function(){
 
+};
+var backButtonHandler = function(){
+    location.reload()
+}
 startButtonE1.addEventListener("click",startQuiz);
 quizButtonsHolder.addEventListener("click",quizButtonHandler)
+highScoreLinkE1.addEventListener("click",toHighScore)
+initialsFormE1.addEventListener("submit",submitScore)
+backButtonE1.addEventListener("click",backButtonHandler)
 
