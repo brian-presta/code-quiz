c7a0169dd3fb30e351b365ea3ee098641908b8e5
brian presta <brian.presta@gmail.comgit commit -m first>
2020-06-12 11:01:20 -0700

    improved style of quiz page

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,5 +1,9 @@
-var timerE1 = document.querySelector("#timer")
+var pageContentE1 = document.querySelector("body")
+var highScoreButtonE1 = pageContentE1.querySelector("#high-score")
+var timerE1 = pageContentE1.querySelector("#timer")
+var startButtonE1 = pageContentE1.querySelector("#start")
 var time = 5
+
 var clockStart = function(){    
     var countDownProcessor = function(){
         timerE1.textContent = "Time: " + time
@@ -10,3 +14,10 @@ var clockStart = function(){
     }
     var timerCountdown = setInterval(countDownProcessor,1000)
 }
+var startQuiz = function(){
+    pageContentE1.querySelector("#intro").style = "display: none"
+    pageContentE1.querySelector("#quiz").style = null
+    clockStart()
+}
+
+startButtonE1.addEventListener("click",startQuiz)
